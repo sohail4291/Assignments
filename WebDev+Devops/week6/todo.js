1b3d2785b6cac98027676516ff214b8b422a524a
Shaik Sohail <128629431+sohail4291@users.noreply.github.com>
2026-06-11 16:06:49 +0530

    updated crud

---
+++ WebDev+Devops/week6/todo.js
@@ -0,0 +1,90 @@
+// let todoIndex = 1;
+// function addItem(){
+//     const element = document.getElementById("input");
+//     const data = element.value;
+//     if(data === ""){
+//         return;
+//     }
+
+//     const newDiv = document.createElement("div");
+//     newDiv.setAttribute("id","todo"+todoIndex);
+
+//     const todoSpan = document.createElement("span");
+//     todoSpan.innerHTML = data;
+//     todoSpan.setAttribute("id","span"+todoIndex);
+
+//     const todoDelete = document.createElement("button");
+//     todoDelete.innerHTML = "Delete Item";
+//     todoDelete.setAttribute("onclick","deleteItem("+todoIndex+")");
+
+//     const todoEdit  = document.createElement("button");
+//     todoEdit.innerHTML = "Edit item";
+//     todoEdit.setAttribute("onclick","editItem("+todoIndex+")");
+
+//     newDiv.appendChild(todoSpan);
+//     newDiv.appendChild(todoDelete);
+//     newDiv.appendChild(todoEdit);
+
+//     const parentDiv = document.getElementById("todo");
+//     parentDiv.appendChild(newDiv);
+//     todoIndex += 1;
+// }
+
+// function deleteItem(index){
+//     let element = document.getElementById("todo"+index);
+//     element.parentElement.removeChild(element);
+// }
+
+// function editItem(index){
+//     const element = document.getElementById("span"+index);
+//     const data = element.textContent;
+//     const newValue = prompt("Enter new data", data);
+//     if(newValue != null){
+//         element.textContent = newValue;
+//     }
+
+// }
+
+
+let index = 1;
+function addItem(){
+    const element = document.getElementById("input");
+    const data = element.value;
+
+    if(data === ""){
+        reuturn;
+    }
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id",index);
+    const todoSpan = document.createElement("span");
+    todoSpan.innerHTML = data;
+    todoSpan.setAttribute("id","span"+index);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "delete item";
+    deleteButton.setAttribute("onclick","deleteItem("+index+")");
+
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "edit button";
+    editButton.setAttribute("onclick","editItem("+index+")");
+
+    newDiv.appendChild(todoSpan);
+    newDiv.appendChild(deleteButton);
+    newDiv.appendChild(editButton);
+    
+    const parentDiv = document.getElementById("todo");
+    parentDiv.appendChild(newDiv);
+
+    index += 1;
+    element.value = "";
+}
+function deleteItem(index){
+    const element = document.getElementById(index);
+    element.parentElement.removeChild(element);
+}
+function editItem(index){
+    const element = document.getElementById("span"+index);
+    const data = element.textContent;
+    const newdata = prompt("enter new data", data);
+    element.textContent = newdata;
+}
